00072bf93219343defbbf6e546bbee1e7acf40c2
minhmo <sipko.tomas@gmail.com>
2019-03-07 15:47:18 +0100

    Socket helper class. Git ignore update. Controller update.

--- src/main/scala/Controller.scala
+++ src/main/scala/Controller.scala
@@ -1,10 +1,22 @@
+import java.io.ObjectOutputStream
+import java.net.Socket
+
+import helper.socketHelper
+
 case class Controller() {
-  def sendInvalidation(objectId: String, node: Node): Unit = {
-    node.neighbours.foreach(n => sendMessage(n))
+
+  /**
+    * Controller method that is responsible for sending invalidations for all neighbors found in @node
+    *
+    * @param invalidation
+    * @param node
+    */
+  def sendInvalidationForAllNeighbours(invalidation: Invalidation, node: Node): Unit = {
+    node.neighbours.foreach(n => sendMessage(n, invalidation))
 
   }
 
-  private def sendMessage(address: Address): Unit = {
-
+  private def sendMessage(address: Address, data: Invalidation): Unit = {
+    socketHelper.send(new Socket(address.host, address.port), data)
   }
 }
