f27af59327e3fbe6a302a6f0feffdf30b893536b
Carsten Griessmann <carsten-griessmann@t-online.de>
2019-03-12 17:57:51 +0100

    added basic webserver to expirement with

---
+++ src/main/scala/UserInterface.scala
@@ -0,0 +1,40 @@
+import java.io.InputStream
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.server.Directives._
+import akka.stream.ActorMaterializer
+
+import scala.io.StdIn
+
+trait FileInterface {
+  def Create(path: String, inputStream: InputStream)
+  def Read(path: String)
+  def Update(path: String, inputStream: InputStream)
+  def Delete(path: String)
+}
+
+object WebServer {
+  def main(args: Array[String]) {
+
+    implicit val system = ActorSystem("my-system")
+    implicit val materializer = ActorMaterializer()
+    implicit val executionContext = system.dispatcher
+
+    val route =
+      path("hello") {
+        get {
+          complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>"))
+        }
+      }
+
+    val bindingFuture = Http().bindAndHandle(route, "localhost", 7070)
+
+    println(s"Server online at http://localhost:7070/\nPress RETURN to stop...")
+    StdIn.readLine() // let it run until user presses return
+    bindingFuture
+      .flatMap(_.unbind()) // trigger unbinding from the port
+      .onComplete(_ => system.terminate()) // and shutdown when done
+  }
+}
